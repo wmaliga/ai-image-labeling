83e968a4bd9dc0e09e7f14323a36f52e4c8eaaee
maliga <wojciech.maliga@nordcloud.com>
2024-04-19 10:56:39 +0200

    UI CloudFront distribution

--- ui/lib/application-stack.ts
+++ ui/lib/application-stack.ts
@@ -2,6 +2,8 @@ import { Construct } from 'constructs';
 import {
   Stack,
   StageProps,
+  aws_cloudfront as cloudfront,
+  aws_cloudfront_origins as origins,
   aws_s3 as s3,
   aws_s3_deployment as s3_deployment,
 } from 'aws-cdk-lib';
@@ -12,16 +14,22 @@ export class ApplicationStack extends Stack {
 
     const bucket = new s3.Bucket(this, id, {
       bucketName: id,
-      accessControl: s3.BucketAccessControl.PUBLIC_READ,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      publicReadAccess: true,
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
+      accessControl: s3.BucketAccessControl.PRIVATE,
     });
 
     new s3_deployment.BucketDeployment(this, `${id}-bucket-deployment`, {
       sources: [s3_deployment.Source.asset('./build')],
       destinationBucket: bucket,
+    });
+
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, `${id}-origin-access-identity`);
+    bucket.grantRead(originAccessIdentity);
+
+    new cloudfront.Distribution(this, `${id}-distribution`, {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket, { originAccessIdentity }),
+      },
     });
   }
 }
